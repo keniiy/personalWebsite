dbb9a0c87c65d090208e38696f971c20e5a9fe82
keniiy <kehindekehinde894@gmail.com>
2021-04-10 05:56:24 +0100

    collect static issue

--- blog/settings.py
+++ blog/settings.py
@@ -244,22 +244,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets")
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "assets")
+#]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+**STATIC_TMP = os.path.join(BASE_DIR, 'static')**
+
+STATIC_URL = '/static/'
+
+**os.makedirs(STATIC_TMP, exist_ok=True)**
+
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 #cloudinary
 cloudinary.config(
   cloud_name = 'kenniy', 
